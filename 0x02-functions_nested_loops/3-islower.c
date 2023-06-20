01979895f0323209d0aa96205d93f0dfbeee684a
Ismail <is.eljamali@gmail.com>
2023-06-20 15:15:03 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _islower check character is lower
+ * _islower - check character is lower
  *
  * @c: check input function
  *
  * Return: return 1 if c is lower
  */
 
-void islowor(int c)
+void _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
