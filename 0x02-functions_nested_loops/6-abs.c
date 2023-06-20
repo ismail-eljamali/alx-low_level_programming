43719f14095b44a6c4e728da0c0dd401588b694e
Ismail <is.eljamali@gmail.com>
2023-06-20 16:05:59 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _abc - function compute the absolue
+ * _abs - function compute the absolue
  *
  * @n: check integertype input function
  *
  * Return: return 0
  */
-int _abc(int n)
+int _abs(int n)
 {
 	if (n < 0)
 		n = (-1) * n;
