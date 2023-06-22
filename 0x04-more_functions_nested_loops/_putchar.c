09080ba0c66d1e820a08a4238ec2e7ffe3d91627
Ismail <is.eljamali@gmail.com>
2023-06-22 13:17:11 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - write character c
+ * @c : character to print
+ *
+ * Return : success 1
+ * error : -1
+ */
+int _putchar (char c)
+{
+	return (write(1, &c, 1));
+}
