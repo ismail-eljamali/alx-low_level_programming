61ebbbc4528e18f3948212cced8ec4b9df5eb7c1
Ismail <is.eljamali@gmail.com>
2023-06-22 13:40:29 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+                return (1);
+        else
+                return (0);
+}
