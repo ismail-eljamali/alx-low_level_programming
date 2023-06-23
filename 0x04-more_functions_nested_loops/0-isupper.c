f428e2e520a92a9737898d85f0a6da76e67f615a
Ismail <is.eljamali@gmail.com>
2023-06-23 14:44:00 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
 /**
  * _isupper - Check if a letter is uppercase
  * @c: Number to be checked
- * Return 0 or 1
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
