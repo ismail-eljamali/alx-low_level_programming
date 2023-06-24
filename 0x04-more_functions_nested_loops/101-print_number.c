e3247bc672877a4a7db1293921351b1d84fa5692
Ismail <is.eljamali@gmail.com>
2023-06-23 18:16:26 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Print integer
+ * @n: integer
+ * Return: empty
+ */
+
+void print_number(int n
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
