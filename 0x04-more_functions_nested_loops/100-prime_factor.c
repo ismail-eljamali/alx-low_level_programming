dbb93732796c35a0d9fe2dd5fff1fd98035b0cfe
Ismail <is.eljamali@gmail.com>
2023-06-23 18:06:42 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+/**
+ * main - print the largest prime
+ * Return: empty
+ */
+
+int main(void)
+{
+	long x, maxy;
+	long n = 612852475143;
+	double square = sr(n);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (n % x == 0)
+		{
+			maxy = n / x;
+		}
+	}
+	printf("%ld\n", maxy);
+	return (0);
+}
