4fa0224bc2810e13ac6fdb7ea90a0877116c8a7d
Ismail <is.eljamali@gmail.com>
2023-06-26 13:13:24 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - Prints string
+ * @str: String 
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
