58ed4007c3b3aa30d5d4f5bfd51627af34a87263
Ismail <is.eljamali@gmail.com>
2023-07-10 04:59:04 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@
 char *_strdup(char *str)
 {
 	char *m;
-	int i = 0
+	int i = 0;
 	int j = 0;
 
 	if (str == NULL)
@@ -23,7 +23,7 @@ char *_strdup(char *str)
 	if (m == NULL)
 		return (NULL);
 
-	for (r = 0 ; str[j]; j++)
+	for (j = 0 ; str[j]; j++)
 		m[j] = str[j];
 
 	return (m);
