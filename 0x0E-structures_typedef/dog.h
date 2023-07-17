6cfcff26696efb77dc7d6375e2edaaab03aefe84
Ismail <is.eljamali@gmail.com>
2023-07-17 07:04:13 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,11 +4,11 @@
 /**
  * init_dog - init a var
  * @d: pointer to struct dog
- * @a: age to initialize
- * @n: name to initialize
+ * @age: age to initialize
+ * @name: name to initialize
  * @owner: owner to initialize
  */
-void init_dog(struct dog *d, char *n, float a, char *o)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
