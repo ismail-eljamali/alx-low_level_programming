dc0e9f49cd6b19598bad9af76856418ae313ec59
Ismail <is.eljamali@gmail.com>
2023-07-20 16:28:08 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,15 +1,15 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers
+ * print_strings - prints numbers
  * @separator: string separator
  * @n: number of argument
  * @...: integers to print
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = n;
+	unsigned int i;
 	char *str;
 	va_list ap;
 
