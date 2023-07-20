1de3ecd931bf91c6d672410c471c5854238a75fe
Ismail <is.eljamali@gmail.com>
2023-07-20 14:11:51 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,7 +7,7 @@
  * @...: integers to print
  * Return: void
  */
-int print_numbers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int i = n;
 	va_list ap;
