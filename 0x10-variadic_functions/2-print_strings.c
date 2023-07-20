48afe4ae2d37e3636bee0f3abf32920b3dd1f0e7
Ismail <is.eljamali@gmail.com>
2023-07-20 16:22:28 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,18 +9,24 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i = n;
+	unsigned int i = n;
 	char *str;
 	va_list ap;
 
-	if (!n)
-	{
-		printf("\n");
-		return;
-	}
 	va_start(ap, n);
-	while (i--)
-		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
-				i ? (separator ? separator : "") : "\n");
-	va_end(ap);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (!str)
+			str = "nil";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
+	}
+printf("\n")
+va_end(ap);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * format_char - format character
+ * @separator: string separator
+ * @ap: argument pointer
+ */
+void format_char(char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	char *str;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
