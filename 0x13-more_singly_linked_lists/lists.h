62309a045848e7592564ff3d3ee81748bd591c33
“Ismail” <is.eljamali@gmail.com>
2023-08-02 07:17:46 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,1 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <strign.h>
+
+/**
+ * struct list_s - single linked list
+ * @str: strinf
+ * @len: length of string
+ * @next: point mode
+ * Description: singly linked
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
